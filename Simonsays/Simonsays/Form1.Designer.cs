23be4ae1dcaf8a97d2264aa9ebf474ba8edd1f28
thijsje89 <33924313+thijsje89@users.noreply.github.com>
2018-06-01 11:31:11 +0200

    Revert "Doc toegevoegd"
    This reverts commit 7cd4c6a854e557a25fb245a00d2a77ab0623b615.

---
+++ Simonsays/Simonsays/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Simonsays
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Red = new System.Windows.Forms.Button();
+            this.Blue = new System.Windows.Forms.Button();
+            this.Yellow = new System.Windows.Forms.Button();
+            this.Green = new System.Windows.Forms.Button();
+            this.scorelabel = new System.Windows.Forms.Label();
+            this.patternlabel = new System.Windows.Forms.Label();
+            this.Purple = new System.Windows.Forms.Button();
+            this.Pink = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Red
+            // 
+            this.Red.Location = new System.Drawing.Point(36, 35);
+            this.Red.Name = "Red";
+            this.Red.Size = new System.Drawing.Size(287, 245);
+            this.Red.TabIndex = 0;
+            this.Red.Text = "red";
+            this.Red.UseVisualStyleBackColor = true;
+            this.Red.Click += new System.EventHandler(this.Red_Click);
+            // 
+            // Blue
+            // 
+            this.Blue.Location = new System.Drawing.Point(340, 35);
+            this.Blue.Name = "Blue";
+            this.Blue.Size = new System.Drawing.Size(287, 245);
+            this.Blue.TabIndex = 1;
+            this.Blue.Text = "blue";
+            this.Blue.UseVisualStyleBackColor = true;
+            this.Blue.Click += new System.EventHandler(this.Blue_Click);
+            // 
+            // Yellow
+            // 
+            this.Yellow.Location = new System.Drawing.Point(649, 35);
+            this.Yellow.Name = "Yellow";
+            this.Yellow.Size = new System.Drawing.Size(287, 245);
+            this.Yellow.TabIndex = 3;
+            this.Yellow.Text = "yellow";
+            this.Yellow.UseVisualStyleBackColor = true;
+            this.Yellow.Click += new System.EventHandler(this.Yellow_Click);
+            // 
+            // Green
+            // 
+            this.Green.Location = new System.Drawing.Point(36, 286);
+            this.Green.Name = "Green";
+            this.Green.Size = new System.Drawing.Size(287, 245);
+            this.Green.TabIndex = 2;
+            this.Green.Text = "green";
+            this.Green.UseVisualStyleBackColor = true;
+            this.Green.Click += new System.EventHandler(this.Green_Click);
+            // 
+            // scorelabel
+            // 
+            this.scorelabel.AutoSize = true;
+            this.scorelabel.Location = new System.Drawing.Point(33, 565);
+            this.scorelabel.Name = "scorelabel";
+            this.scorelabel.Size = new System.Drawing.Size(61, 17);
+            this.scorelabel.TabIndex = 4;
+            this.scorelabel.Text = "Score: 0";
+            // 
+            // patternlabel
+            // 
+            this.patternlabel.AutoSize = true;
+            this.patternlabel.Location = new System.Drawing.Point(337, 565);
+            this.patternlabel.Name = "patternlabel";
+            this.patternlabel.Size = new System.Drawing.Size(64, 17);
+            this.patternlabel.TabIndex = 5;
+            this.patternlabel.Text = "Reeks: 0";
+            this.patternlabel.Click += new System.EventHandler(this.patternlabel_Click);
+            // 
+            // Purple
+            // 
+            this.Purple.Location = new System.Drawing.Point(340, 286);
+            this.Purple.Name = "Purple";
+            this.Purple.Size = new System.Drawing.Size(287, 245);
+            this.Purple.TabIndex = 6;
+            this.Purple.Text = "purple";
+            this.Purple.UseVisualStyleBackColor = true;
+            this.Purple.Click += new System.EventHandler(this.Purple_Click);
+            // 
+            // Pink
+            // 
+            this.Pink.Location = new System.Drawing.Point(649, 286);
+            this.Pink.Name = "Pink";
+            this.Pink.Size = new System.Drawing.Size(287, 245);
+            this.Pink.TabIndex = 7;
+            this.Pink.Text = "pink";
+            this.Pink.UseVisualStyleBackColor = true;
+            this.Pink.Click += new System.EventHandler(this.Pink_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 597);
+            this.Controls.Add(this.Pink);
+            this.Controls.Add(this.Purple);
+            this.Controls.Add(this.patternlabel);
+            this.Controls.Add(this.scorelabel);
+            this.Controls.Add(this.Yellow);
+            this.Controls.Add(this.Green);
+            this.Controls.Add(this.Blue);
+            this.Controls.Add(this.Red);
+            this.Name = "Form1";
+            this.Text = "Simon Says";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Red;
+        private System.Windows.Forms.Button Blue;
+        private System.Windows.Forms.Button Yellow;
+        private System.Windows.Forms.Button Green;
+        private System.Windows.Forms.Label scorelabel;
+        private System.Windows.Forms.Label patternlabel;
+        private System.Windows.Forms.Button Purple;
+        private System.Windows.Forms.Button Pink;
+    }
+}
+
---
+++ Simonsays/Simonsays/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Media;
+namespace Simonsays
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int onInList = 0;
+        List<int> pattern = new List<int>(); //
+        Random rand = new Random(); //de random knop die er wordt opgelicht wordt
+        bool playingBack = false; 
+
+        private void Red_Click(object sender, EventArgs e)
+        {
+            testCorrect(0);
+            //als deze knop ingedrukt word geeft hij een een 0 door aan de functie testCorrect
+        }
+
+        private void Blue_Click(object sender, EventArgs e)
+        {
+            testCorrect(1);
+            //als deze knop ingedrukt word geeft hij een een 1 door aan de functie testCorrect
+        }
+
+        private void Yellow_Click(object sender, EventArgs e)
+        {
+            testCorrect(2);
+            //als deze knop ingedrukt word geeft hij een een 2 door aan de functie testCorrect
+        }
+
+        private void Green_Click(object sender, EventArgs e)
+        {
+            testCorrect(3);
+            //als deze knop ingedrukt word geeft hij een een 3 door aan de functie testCorrect
+        }
+
+        void testCorrect(int color)
+        {
+            //voer de functie playingBack uit als deze aangeroept kan worden
+            if (playingBack)
+                return;
+            //als de kleur(en) overeenkomt met de kleur(en) uit het array voeg er een aan toe.
+            if (pattern[onInList] == color)
+            {
+                onInList++;
+            }
+            else //als de kleur(en) niet overeenkomt
+            {
+                MessageBox.Show("Final Score: " + pattern.Count.ToString()); //geeft een messagebox weer die je final score zegt
+                onInList = 0; //reset de lijst
+                pattern = new List<int>();
+                new Thread(playback).Start();
+            }
+            if (onInList >= pattern.Count) //als de lijst groter is dan het huidige patroon doe dit
+            {
+                pattern.Add(rand.Next(0, 6)); //voeg een random nummer tussen 0 en 6 toe aan de lijst
+                onInList = 0; 
+                new Thread(playback).Start();
+            }
+            scorelabel.Text = ("Score: " + pattern.Count.ToString()); //laat de huidige score zien
+            patternlabel.Text = ("Item within pattern: " + onInList.ToString()); //laat de huidige items in de reeks zien
+        }
+
+        void playback() //deze functie gebeurt als de computer de reeks laat zien die nagespeeld moet worden
+        {
+            playingBack = true;
+            foreach(int color in pattern){ //voor elke kleur in de lijst doe een van de volgende cases
+                switch (color)
+                {
+                    case 0: //knop 1
+                        Red.BackColor = Color.Red;
+                        Thread.Sleep(500);
+                        Red.BackColor = Color.Transparent;
+                        break;
+
+                    case 1: //knop 2
+                        Blue.BackColor = Color.Blue;
+                        Thread.Sleep(500);
+                        Blue.BackColor = Color.Transparent;
+                        break;
+
+                    case 2: //knop 3
+                        Yellow.BackColor = Color.Yellow;
+                        Thread.Sleep(500);
+                        Yellow.BackColor = Color.Transparent;
+                        break;
+
+                    case 3: //knop 4
+                        Green.BackColor = Color.Green;
+                        Thread.Sleep(500);
+                        Green.BackColor = Color.Transparent;
+                        break;
+
+                    case 4: //knop 5
+                        Purple.BackColor = Color.Purple;
+                        Thread.Sleep(500);
+                        Purple.BackColor = Color.Transparent;
+                        break;
+
+                    case 5: //knop 6
+                        Pink.BackColor = Color.Pink;
+                        Thread.Sleep(500);
+                        Pink.BackColor = Color.Transparent;
+                        break;
+                }
+                Thread.Sleep(200); //wacht even voordat de volgende reeks wordt laten zien
+            }
+            playingBack = false;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pattern.Add(rand.Next(0, 6)); //voeg een random nummer tussen 1 en 6 toe
+            new Thread(playback).Start();
+        }
+
+        private void Purple_Click(object sender, EventArgs e)
+        {
+            testCorrect(4);
+            //als deze knop ingedrukt word geeft hij een een 4 door aan de functie testCorrect
+        }
+
+        private void Pink_Click(object sender, EventArgs e)
+        {
+            testCorrect(5);
+            //als deze knop ingedrukt word geeft hij een een 5 door aan de functie testCorrect
+        }
+
+        private void patternlabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
