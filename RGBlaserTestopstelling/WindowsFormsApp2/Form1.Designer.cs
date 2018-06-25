5a2a9388e93b017439638094075a15d143bdb572
thijsje89 <33924313+thijsje89@users.noreply.github.com>
2018-06-25 09:37:12 +0200

    Add files via upload

---
+++ RGBlaserTestopstelling/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Font = new System.Drawing.Font("Gill Sans MT", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Snow;
+            this.button1.Location = new System.Drawing.Point(666, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(300, 300);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Blauw";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.Font = new System.Drawing.Font("Gill Sans MT", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Snow;
+            this.button2.Location = new System.Drawing.Point(54, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(300, 300);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Rood";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Green;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button3.Font = new System.Drawing.Font("Gill Sans MT", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Snow;
+            this.button3.Location = new System.Drawing.Point(360, 53);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(300, 300);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Groen";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Yellow;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button4.Font = new System.Drawing.Font("Gill Sans MT", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(54, 359);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(300, 300);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Geel";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Cyan;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button5.Font = new System.Drawing.Font("Gill Sans MT", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.Black;
+            this.button5.Location = new System.Drawing.Point(666, 359);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(300, 300);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Cyaan";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Magenta;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button6.Font = new System.Drawing.Font("Gill Sans MT", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.Black;
+            this.button6.Location = new System.Drawing.Point(360, 359);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(300, 300);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Magenta";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.White;
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button7.Font = new System.Drawing.Font("Gill Sans MT", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.Color.Black;
+            this.button7.Location = new System.Drawing.Point(972, 359);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(300, 300);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Wit";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1343, 688);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ RGBlaserTestopstelling/laser.ino
@@ -0,0 +1,99 @@
+//Definiering van de pinnen aangesloten op de laser, dit zijn PWM poorten voor het dimmen
+const int red = 3;
+const int green = 5;
+const int blue = 6;
+
+const int buttonWhite = 8;
+const int buttonRed = 9;
+
+//De variablen voor het bijhouden van de verschillende kleuren en dimwaarden
+char receivedChar;
+String receivedString;
+
+int color;
+int dimFactor;
+
+
+void setup() {
+  //pinnen instellen als INPUT OF OUTPUT
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+
+  pinMode(buttonWhite, INPUT);
+  pinMode(buttonRed, INPUT);
+
+  //serial port openen voor communicatie met de PC
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  //wanneer er een bericht binnenkomt, lees het hele bericht
+  //en zet het in de int color
+  if (Serial.available() > 0) {
+    color = Serial.read();
+  }
+
+  //lees allebei de knoppen uit
+  bool buttonStateRed = digitalRead(buttonRed);
+  bool buttonStateWhite = digitalRead(buttonWhite);
+
+  //als de verhoog knop wordt ingedrukt, verhoog de dimFactor
+  if(buttonStateRed == HIGH && dimFactor < 255){
+    dimFactor++;
+    delay(10);
+    Serial.println(dimFactor);
+  }
+  //als de verlaag knop wordt ingedrukt, verlaag de dimFactor
+  if(buttonStateWhite == HIGH && dimFactor > 0){
+    dimFactor--;
+    delay(10);
+    Serial.println(dimFactor);
+  }
+
+  //lees het seriële bericht in color
+  //en stel aan de hand daarvan de kleur in, bij de juiste dimFactor
+  switch (color) {
+    case '1':
+      analogWrite(red, dimFactor);
+      analogWrite(green, 0);
+      analogWrite(blue, 0);
+      break;
+    case '2':
+      analogWrite(red, 0);
+      analogWrite(green, dimFactor);
+      analogWrite(blue, 0);
+      break;
+    case '3':
+      analogWrite(red, 0);
+      analogWrite(green, 0);
+      analogWrite(blue, dimFactor);
+      break;
+    case '4':
+      analogWrite(red, dimFactor);
+      analogWrite(green, dimFactor);
+      analogWrite(blue, 0);
+      break;
+    case '5':
+      analogWrite(red, dimFactor);
+      analogWrite(green, 0);
+      analogWrite(blue, dimFactor);
+      break;
+    case '6':
+      analogWrite(red, 0);
+      analogWrite(green, dimFactor);
+      analogWrite(blue, dimFactor);
+      break;
+    case '7':
+      analogWrite(red, dimFactor);
+      analogWrite(green, dimFactor);
+      analogWrite(blue, dimFactor);
+      break;
+    default:
+      analogWrite(red, 0);
+      analogWrite(green, 0);
+      analogWrite(blue, 0);
+    break;
+  }
+}
